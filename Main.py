0b8a760aa459406eee092c3a1402c1d4dac984ba
natanmorais <morais_natan@hotmail.com>
2020-07-27 14:31:37 -0300

    Criado esqueleto do projeto. Adicionados arquivos Reader.py (Classe do leitor de arquivos) e Main.py (Arquivo inicial de execuçao).

---
+++ Main.py
@@ -0,0 +1,6 @@
+def main():
+    #TODO: Create Main Method
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ Reader.py
@@ -0,0 +1,8 @@
+#Class Reader
+class Reader:
+    def __init__(self):
+        pass
+
+    def readFile(self, fullPath):
+        #TODO: Create reading method
+        return
